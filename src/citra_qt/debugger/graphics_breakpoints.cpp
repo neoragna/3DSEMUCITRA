emmauss/citra

Implement 4 shader units and geometry shaders

src/citra_qt/debugger/graphics_breakpoints.cpp
@@ -45,6 +45,7 @@ QVariant BreakPointModel::data(const QModelIndex& index, int role) const
                 { Pica::DebugContext::Event::IncomingPrimitiveBatch, tr("Incoming primitive batch") },
                 { Pica::DebugContext::Event::FinishedPrimitiveBatch, tr("Finished primitive batch") },
                 { Pica::DebugContext::Event::RunVS, tr("Vertex shader invocation") },
+                { Pica::DebugContext::Event::RunGS, tr("Geometry shader invocation") },
                 { Pica::DebugContext::Event::IncomingDisplayTransfer, tr("Incoming display transfer") },
                 { Pica::DebugContext::Event::GSPCommandProcessed, tr("GSP command processed") },
                 { Pica::DebugContext::Event::BufferSwapped, tr("Buffers swapped") }

src/video_core/command_processor.cpp
@@ -135,7 +135,7 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
                     if (immediate_attribute_id >= regs.vs.num_input_attributes+1) {
                         immediate_attribute_id = 0;
 
-                        Shader::UnitState<false> shader_unit;
+                        auto& shader_unit = Shader::GetShaderUnit(false);
                         g_state.vs.Setup();
 
                         // Send to vertex shader
@@ -295,8 +295,11 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
             unsigned int vertex_cache_pos = 0;
             vertex_cache_ids.fill(-1);
 
-            Shader::UnitState<false> shader_unit;
+            auto& vs_shader_unit = Shader::GetShaderUnit(false);
             g_state.vs.Setup();
+
+            auto& gs_unit_state = Shader::GetShaderUnit(true);
+            g_state.gs.Setup();
 
             for (unsigned int index = 0; index < regs.num_vertices; ++index)
             {
@@ -379,9 +382,9 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
 
                     // Send to vertex shader
                     if (g_debug_context)
-                        g_debug_context->OnEvent(DebugContext::Event::RunVS, (void*)&input);
-                    g_state.vs.Run(shader_unit, input, attribute_config.GetNumTotalAttributes(), g_state.regs.vs);
-                    output_registers = shader_unit.output_registers;
+                        g_debug_context->OnEvent(DebugContext::Event::RunVS, static_cast<void*>(&input));
+                    g_state.vs.Run(vs_shader_unit, input, attribute_config.GetNumTotalAttributes(), g_state.regs.vs);
+                    output_registers = vs_shader_unit.output_registers;
 
                     if (is_indexed) {
                         vertex_cache[vertex_cache_pos] = output_registers;
@@ -390,17 +393,56 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
                     }
                 }
 
-                // Retreive vertex from register data
-                Shader::OutputVertex output_vertex = output_registers.ToVertex(regs.vs);
-
-                // Send to renderer
+                // Helper to send triangle to renderer
                 using Pica::Shader::OutputVertex;
                 auto AddTriangle = [](
                         const OutputVertex& v0, const OutputVertex& v1, const OutputVertex& v2) {
                     VideoCore::g_renderer->Rasterizer()->AddTriangle(v0, v1, v2);
                 };
 
-                primitive_assembler.SubmitVertex(output_vertex, AddTriangle);
+                if (Shader::UseGS()) {
+
+                    auto& regs = g_state.regs;
+                    auto& gs_regs = g_state.regs.gs;
+                    auto& gs_buf = g_state.gs_input_buffer;
+
+                    // Vertex Shader Outputs are converted into Geometry Shader inputs by filling up a buffer
+                    // For example, if we have a geoshader that takes 6 inputs, and the vertex shader outputs 2 attributes
+                    // It would take 3 vertices to fill up the Geometry Shader buffer
+                    unsigned int gs_input_count = gs_regs.num_input_attributes + 1;
+                    unsigned int vs_output_count = regs.vs_outmap_total2 + 1;
+                    ASSERT_MSG(regs.vs_outmap_total1 == regs.vs_outmap_total2, "VS_OUTMAP_TOTAL1 and VS_OUTMAP_TOTAL2 don't match!");
+                    // copy into the geoshader buffer
+                    for (unsigned int i = 0; i < vs_output_count; i++) {
+                        if (gs_buf.index >= gs_input_count) {
+                            // TODO(ds84182): LOG_ERROR()
+                            ASSERT_MSG(false, "Number of GS inputs (%d) is not divisible by number of VS outputs (%d)",
+                                        gs_input_count, vs_output_count);
+                            continue;
+                        }
+                        gs_buf.buffer.attr[gs_buf.index++] = output_registers.value[i];
+                    }
+
+                    if (gs_buf.index >= gs_input_count) {
+
+                        // b15 will be false when a new primitive starts and then switch to true at some point
+                        //TODO: Test how this works exactly on hardware
+                        g_state.gs.uniforms.b[15] |= (index > 0);
+
+                        // Process Geometry Shader
+                        if (g_debug_context)
+                            g_debug_context->OnEvent(DebugContext::Event::RunGS, static_cast<void*>(&gs_buf.buffer));
+                        gs_unit_state.emit_triangle_callback = AddTriangle;
+                        g_state.gs.Run(gs_unit_state, gs_buf.buffer, gs_input_count, regs.gs);
+                        gs_unit_state.emit_triangle_callback = nullptr;
+
+                        gs_buf.index = 0;
+                    }
+                } else {
+                    Shader::OutputVertex output_vertex = output_registers.ToVertex(regs.vs);
+                    primitive_assembler.SubmitVertex(output_vertex, AddTriangle);
+                }
+
             }
 
             for (auto& range : memory_accesses.ranges) {

neoragna/3DSEMUCITRA

src/citra_qt/debugger/graphics_breakpoints.cpp
@@ -45,6 +45,7 @@ QVariant BreakPointModel::data(const QModelIndex& index, int role) const
                 { Pica::DebugContext::Event::IncomingPrimitiveBatch, tr("Incoming primitive batch") },
                 { Pica::DebugContext::Event::FinishedPrimitiveBatch, tr("Finished primitive batch") },
                 { Pica::DebugContext::Event::VertexShaderInvocation, tr("Vertex shader invocation") },
+                { Pica::DebugContext::Event::GeometryShaderInvocation, tr("Geometry shader invocation") },
                 { Pica::DebugContext::Event::IncomingDisplayTransfer, tr("Incoming display transfer") },
                 { Pica::DebugContext::Event::GSPCommandProcessed, tr("GSP command processed") },
                 { Pica::DebugContext::Event::BufferSwapped, tr("Buffers swapped") }

src/video_core/command_processor.cpp
@@ -139,7 +139,7 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
                     if (immediate_attribute_id >= regs.vs.num_input_attributes+1) {
                         immediate_attribute_id = 0;
 
-                        Shader::UnitState<false> shader_unit;
+                        auto& shader_unit = Shader::GetShaderUnit(false);
                         g_state.vs.Setup();
 
                         // Send to vertex shader
@@ -232,8 +232,11 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
             unsigned int vertex_cache_pos = 0;
             vertex_cache_ids.fill(-1);
 
-            Shader::UnitState<false> shader_unit;
+            auto& vs_shader_unit = Shader::GetShaderUnit(false);
             g_state.vs.Setup();
+
+            auto& gs_unit_state = Shader::GetShaderUnit(true);
+            g_state.gs.Setup();
 
             for (unsigned int index = 0; index < regs.num_vertices; ++index)
             {
@@ -270,8 +273,8 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
                     // Send to vertex shader
                     if (g_debug_context)
                         g_debug_context->OnEvent(DebugContext::Event::VertexShaderInvocation, (void*)&input);
-                    g_state.vs.Run(shader_unit, input, loader.GetNumTotalAttributes(), regs.vs);
-                    output_registers = shader_unit.output_registers;
+                    g_state.vs.Run(vs_shader_unit, input, loader.GetNumTotalAttributes(), regs.vs);
+                    output_registers = vs_shader_unit.output_registers;
 
                     if (is_indexed) {
                         vertex_cache[vertex_cache_pos] = output_registers;
@@ -280,17 +283,56 @@ static void WritePicaReg(u32 id, u32 value, u32 mask) {
                     }
                 }
 
-                // Retreive vertex from register data
-                Shader::OutputVertex output_vertex = output_registers.ToVertex(regs.vs);
-
-                // Send to renderer
+                // Helper to send triangle to renderer
                 using Pica::Shader::OutputVertex;
                 auto AddTriangle = [](
                         const OutputVertex& v0, const OutputVertex& v1, const OutputVertex& v2) {
                     VideoCore::g_renderer->Rasterizer()->AddTriangle(v0, v1, v2);
                 };
 
-                primitive_assembler.SubmitVertex(output_vertex, AddTriangle);
+                if (Shader::UseGS()) {
+
+                    auto& regs = g_state.regs;
+                    auto& gs_regs = g_state.regs.gs;
+                    auto& gs_buf = g_state.gs_input_buffer;
+
+                    // Vertex Shader Outputs are converted into Geometry Shader inputs by filling up a buffer
+                    // For example, if we have a geoshader that takes 6 inputs, and the vertex shader outputs 2 attributes
+                    // It would take 3 vertices to fill up the Geometry Shader buffer
+                    unsigned int gs_input_count = gs_regs.num_input_attributes + 1;
+                    unsigned int vs_output_count = regs.vs_outmap_total2 + 1;
+                    ASSERT_MSG(regs.vs_outmap_total1 == regs.vs_outmap_total2, "VS_OUTMAP_TOTAL1 and VS_OUTMAP_TOTAL2 don't match!");
+                    // copy into the geoshader buffer
+                    for (unsigned int i = 0; i < vs_output_count; i++) {
+                        if (gs_buf.index >= gs_input_count) {
+                            // TODO(ds84182): LOG_ERROR()
+                            ASSERT_MSG(false, "Number of GS inputs (%d) is not divisible by number of VS outputs (%d)",
+                                        gs_input_count, vs_output_count);
+                            continue;
+                        }
+                        gs_buf.buffer.attr[gs_buf.index++] = output_registers.value[i];
+                    }
+
+                    if (gs_buf.index >= gs_input_count) {
+
+                        // b15 will be false when a new primitive starts and then switch to true at some point
+                        //TODO: Test how this works exactly on hardware
+                        g_state.gs.uniforms.b[15] |= (index > 0);
+
+                        // Process Geometry Shader
+                        if (g_debug_context)
+                            g_debug_context->OnEvent(DebugContext::Event::GeometryShaderInvocation, static_cast<void*>(&gs_buf.buffer));
+                        gs_unit_state.emit_triangle_callback = AddTriangle;
+                        g_state.gs.Run(gs_unit_state, gs_buf.buffer, gs_input_count, regs.gs);
+                        gs_unit_state.emit_triangle_callback = nullptr;
+
+                        gs_buf.index = 0;
+                    }
+                } else {
+                    Shader::OutputVertex output_vertex = output_registers.ToVertex(regs.vs);
+                    primitive_assembler.SubmitVertex(output_vertex, AddTriangle);
+                }
+
             }
 
             for (auto& range : memory_accesses.ranges) {