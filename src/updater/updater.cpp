eeac6769b2734d4f2eb32da94fc8cd5cffceae28
James Rowe <jroweboy@gmail.com>
2016-09-05 22:38:38 -0600

    Compile version into updater. Clean up updater

--- src/updater/updater.cpp
+++ src/updater/updater.cpp
@@ -3,27 +3,22 @@
 #include <regex>
 #include <windows.h>
 
-#include <cpr/cpr.h>
-
 // Have to include getopt in the compilation unit or else its compiled with the wrong runtime
+#include <cpr/cpr.h>
 #include "getopt.h"
+#include "tagname.h"
 
-void startProcessWrapper(std::string name, std::string args, boolean wait) { //) {//, boolean hidden,
+void startProcessWrapper(std::string name, std::string args, boolean wait) {
     HANDLE stdout_handle = NULL;
     STARTUPINFO startupInfo={sizeof(startupInfo)};
-    // if (hidden) {
-        startupInfo.dwFlags = STARTF_USESHOWWINDOW;
-        startupInfo.wShowWindow = SW_HIDE;
-        startupInfo.hStdOutput = stdout_handle;
-    // }
+    startupInfo.dwFlags = STARTF_USESHOWWINDOW;
+    startupInfo.wShowWindow = SW_HIDE;
+    startupInfo.hStdOutput = stdout_handle;
     PROCESS_INFORMATION processInfo;
     LPSTR cmdArgs = const_cast<char *>(args.c_str());
     if (!CreateProcess(name.c_str(), cmdArgs,
-        NULL, NULL, FALSE,
-        //(hidden) ? CREATE_NO_WINDOW : 0
-        CREATE_NO_WINDOW
-        , NULL, NULL, &startupInfo, &processInfo)
-    ) {
+        NULL, NULL, FALSE, CREATE_NO_WINDOW,
+        NULL, NULL, &startupInfo, &processInfo)) {
         std::cout << "CreateProcess failed (" << GetLastError() << ")" << std::endl;
         CloseHandle(processInfo.hProcess);
         CloseHandle(processInfo.hThread);
@@ -36,20 +31,18 @@ void startProcessWrapper(std::string name, std::string args, boolean wait) { //)
 }
 
 void displayWelcome() {
-    MessageBoxA(NULL, "Welcome to Lemon, the Citra Beta builds! \n\nPlease understand that these builds are made "
-     "automatically by a bot, so don't expect any support for them on the forum or github.",
-     "Lemon - The Citra Beta", MB_OK | MB_ICONINFORMATION);
+    MessageBoxA(NULL, "You are currently running the Citra Nightly Unstable branch.\n"
+        "This is a build of Citra with additional changes that have not yet been fully tested or deemed stable."
+        "As such, please do not report bugs against this build.\n\n"
+        "If you believe you've found a bug, please reattempt on our nightly stable builds.\n\n"
+        "Citra will automatically update when new nightly builds are released.",
+    "Lemon - The Citra Beta", MB_OK | MB_ICONINFORMATION);
 }
-
-// void startCitra() {
-//     startProcessWrapper("citra-qt.exe", "", false, true);
-// }
 
 void installMSVCRuntime(const char* msvc_redist_exe) {
     // this updater is linked statically with the runtime so it should be safe to run on any system
     // TODO change this to support 32 bit as well?
     // check to see if they have a recent enough version of the runtime
-
     WCHAR szBuffer[512];
     DWORD dwBufferSize = sizeof(szBuffer);
     HKEY hKey;
@@ -92,53 +85,43 @@ int main(int argc, char** argv) {
         { 0, 0, 0, 0 }
     };
 
-    // Get the path to the vcredist
+    // Get the path to the main applications
     char buffer[MAX_PATH];
     GetModuleFileName(NULL, buffer, MAX_PATH);
     std::string fullpath = std::string(buffer);
     std::string path = fullpath.substr(0, fullpath.find_last_of("\\/"));
     std::string msvc_redist_exe = path + "\\vcredist_x64.exe";
     std::string updater_exe = path.substr(0, path.find_last_of("\\/") + 1) + "Update.exe";
-    std::cout << fullpath << std::endl;
-    std::cout << path << std::endl;
-    std::cout << msvc_redist_exe << std::endl;
-    std::cout << updater_exe << std::endl;
     while (optind < argc) {
         char arg = getopt_long(argc, argv, "fi:o:u:r:", long_options, &option_index);
         if (arg != -1) {
             switch (arg) {
             case 'f':
-                std::cout << "Displayed Welcome" << std::endl;
+                // First time run. Display a welcome message and exit quickly.
                 displayWelcome();
-                std::cout << "Start citra" << std::endl;
-                startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false); // true, false);
+                startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false);
                 return 0;
             case 'i':
-                std::cout << "Trying to install msvc redist" << std::endl;
+                // On install. Do what is needed and exist fast.
                 installMSVCRuntime(msvc_redist_exe.c_str());
-                std::cout << "Trying to make a shortcut" << std::endl;
-                startProcessWrapper(updater_exe, "Update.exe --createShortcut=updater.exe", true); //, true, false);
+                startProcessWrapper(updater_exe, "Update.exe --createShortcut=updater.exe", true);
                 return 0;
             case 'o':
-                std::cout << "Obsolete do nothing?" << std::endl;
-                startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false); // true, false);
+                // Obsolete. Not sure what to use it for.
                 return 0;
             case 'u':
-                std::cout << "Update... do nothing?" << std::endl;
-                startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false); // true, false);
+                // Update. Fresh version so just run it an exit.
+                startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false);
                 return 0;
             case 'r':
-                std::cout << "Uninstalling. Remove shortcut" << std::endl;
-                startProcessWrapper(updater_exe, "Update.exe --removeShortcut=updater.exe", true); //, true, false);
+                // This throws an error when everything is somehow gone when it tries to remove the shortcut...
+                startProcessWrapper(updater_exe, "Update.exe --removeShortcut=updater.exe", true);
                 return 0;
             }
         } else {
             optind++;
         }
     }
-    // Normal Run so just start citra-qt
-    startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false); //, true, true);
-
     // check for updates in the background
     // fetch the latest tag number from github
     auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/jroweboy/lemon/releases/latest"});
@@ -148,10 +131,19 @@ int main(int argc, char** argv) {
         std::regex tag_regex(".*\"tag_name\":\\s*\"(.*)\".*");
         if (std::regex_search(r.text, match, tag_regex)) {
             std::string tag_name = match[1];
-            // download the latest version and get it ready
-            startProcessWrapper(updater_exe, "Update.exe --update=https://github.com/jroweboy/lemon/releases/download/"+tag_name, true);
-            // and we are done! Next boot it'll point to the new version.
+            // if the version is the same, just start citra
+            if (tag_name != Updater::tag_name) {
+                // display a loading animation ??
+                std::cout << "Hang tight! Downloading the latest updates just for you" << std::endl;
+                std::cout << "Citra will launch as soon as the update is finished" << std::endl;
+                // download the latest version and get it ready
+                startProcessWrapper(updater_exe, "Update.exe --update=https://github.com/jroweboy/lemon/releases/download/"+tag_name, true);
+                // and we are done! Next boot it'll point to the new version.
+                return 0;
+            }
         }
     }
+    // Normal Run so just start citra-qt
+    startProcessWrapper(updater_exe, "Update.exe --processStart=citra-qt.exe", false);
     return 0;
 }
