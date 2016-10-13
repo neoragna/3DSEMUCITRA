771e0ab71d27fdf9d28a340acd8882383283e497
Anon <makotech222@gmail.com>
2016-10-12 19:23:42 -0500

    # This is a combination of 2 commits. # The first commit's message is:
    Add Gateway cheat support. Also fix code style
    
    # This is the commit message #2:
    
    fix bad rebase

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -12,6 +12,7 @@
 #include <QMessageBox>
 #include <QtGui>
 #include "citra_qt/bootmanager.h"
+#include "citra_qt/cheat_gui.h"
 #include "citra_qt/config.h"
 #include "citra_qt/configure_dialog.h"
 #include "citra_qt/debugger/callstack.h"
@@ -145,6 +146,7 @@ GMainWindow::GMainWindow() : config(new Config()), emu_thread(nullptr) {
     microProfileDialog->restoreGeometry(UISettings::values.microprofile_geometry);
     microProfileDialog->setVisible(UISettings::values.microprofile_visible);
 #endif
+    ui.action_Cheats->setEnabled(false);
 
     game_list->LoadInterfaceLayout();
 
@@ -168,6 +170,7 @@ GMainWindow::GMainWindow() : config(new Config()), emu_thread(nullptr) {
     connect(game_list, SIGNAL(GameChosen(QString)), this, SLOT(OnGameListLoadFile(QString)),
             Qt::DirectConnection);
     connect(ui.action_Configure, SIGNAL(triggered()), this, SLOT(OnConfigure()));
+    connect(ui.action_Cheats, SIGNAL(triggered()), this, SLOT(OnCheats()));
     connect(ui.action_Load_File, SIGNAL(triggered()), this, SLOT(OnMenuLoadFile()),
             Qt::DirectConnection);
     connect(ui.action_Load_Symbol_Map, SIGNAL(triggered()), this, SLOT(OnMenuLoadSymbolMap()));
@@ -401,6 +404,7 @@ void GMainWindow::ShutdownGame() {
     ui.action_Start->setText(tr("Start"));
     ui.action_Pause->setEnabled(false);
     ui.action_Stop->setEnabled(false);
+    ui.action_Cheats->setEnabled(false);
     render_window->hide();
     game_list->show();
 
@@ -498,6 +502,7 @@ void GMainWindow::OnStartGame() {
 
     ui.action_Start->setEnabled(false);
     ui.action_Start->setText(tr("Continue"));
+    ui.action_Cheats->setEnabled(true);
 
     ui.action_Pause->setEnabled(true);
     ui.action_Stop->setEnabled(true);
@@ -548,6 +553,11 @@ void GMainWindow::OnConfigure() {
         render_window->ReloadSetKeymaps();
         config->Save();
     }
+}
+
+void GMainWindow::OnCheats() {
+    CheatDialog cheatDialog(this);
+    cheatDialog.exec();
 }
 
 void GMainWindow::OnCreateGraphicsSurfaceViewer() {
